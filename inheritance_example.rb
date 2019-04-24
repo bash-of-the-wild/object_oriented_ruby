1d2b86c15cc35f0582bc8e73dc9b7e0c3a9c3f4f
JackMarx <seriousfools@gmail.com>
2019-04-24 11:12:46 -0500

    super

--- inheritance_example.rb
+++ inheritance_example.rb
@@ -18,24 +18,48 @@ class Vehicle
 end
 
 class Car < Vehicle
+  def initialize(input_options)
+    super()
+    @make = input_options[:make]
+    @model = input_options[:model]
+    @fuel = input_options[:fuel]
+  end
+
   def honk_horn
     puts "Beeeeeeep!"
   end
 end
 
 class Bike < Vehicle
+  def initialize(input_options)
+    super()
+    @type = input_options[:type]
+    @weight = input_options[:weight]
+    @gears = input_options[:gears]
+  end
+
   def ring_bell
     puts "Ring ring!"
   end
 end
 
-car = Car.new
-bike = Bike.new
+car = Car.new(
+              fuel: 10,
+              model: "Cooper S",
+              make: "Mini"
+              )
 
+bike = Bike.new(
+                type: "Mountain",
+                gears: 16,
+                weight: 27
+                )
+
+vehicle = Vehicle.new
+
+p vehicle
 p car
 p bike
 
-bike.ring_bell
-car.honk_horn
 
 
